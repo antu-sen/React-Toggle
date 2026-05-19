b9eeea5f63953cde16c56d9b46a0384acb89af40
Antu Sen <antusen787@gmail.com>
2026-05-19 18:11:20 +0600

    added pricinCard component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <header>
         <Navbar />
+        
       </header>
       <main>
         <Suspense
---
+++ src/components/PricingCard/PricingCard.jsx
@@ -0,0 +1,10 @@
+const PricingCard = () => {
+    return (
+        <div>
+            <h1>PricingCard</h1>
+            
+        </div>
+    );
+};
+
+export default PricingCard;

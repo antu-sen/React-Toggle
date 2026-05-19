3c37ca8df981d15e1bbcebe1afb66e293beccb6f
Antu Sen <antusen787@gmail.com>
2026-05-19 17:29:46 +0600

    responsive navbar add

--- src/components/NavBar/Link.jsx
+++ src/components/NavBar/Link.jsx
@@ -1,7 +1,7 @@
 
 const Link = ({route}) => {
     return (
-        <li className="mr-12">
+        <li className="px-4 lg:mr-8 hover:bg-amber-500">
             <a className="text-gray-700 font-medium hover:text-blue-600 transition duration-300" href={route.path}>{route.name}</a>
         </li>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Link from "./NavBar/Link";
-import { TextAlignEnd } from "lucide-react";
+import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 
 const navigationData = [
@@ -35,25 +35,38 @@ const navigationData = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
+  const links =   navigationData.map((route) => (
+    <Link key={route.id} route={route}></Link>
+  ))
+
   return (
-    <nav className="flex justify-between items-center py-2 px-20">
+    <nav className="flex justify-between items-center md:px-20 py-2 px-4">
       <span className="flex" onClick={() => setOpen(!open)}>
         {open ? (
           <X className="md:hidden"></X>
         ) : (
-          <TextAlignEnd className="md:hidden"></TextAlignEnd>
+          <Menu className="md:hidden"></Menu>
         )}
 
-        <h3 className="text-3xl font-extrabold tracking-wide text-blue-600 cursor-pointer">
+        <ul className={`md:hidden absolute  duration-1000
+          ${open ? "top-10" : "-top-40" }
+            bg-amber-300`}>
+          {
+            links
+          }
+        </ul>
+
+        <h3 className=" hidden md:block md:text-3xl md:font-extrabold  tracking-wide text-blue-600 cursor-pointer">
           My<span className="text-black">NavBar</span>
         </h3>
       </span>
-      <ul className="flex justify-center items-center">
-        {navigationData.map((route) => (
-          <Link key={route.id} route={route}></Link>
-        ))}
+      <ul className="md:flex hidden justify-center items-center">
+        {
+          links
+      
+        }
       </ul>
-      <button className="px-5 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300 cursor-pointer">
+      <button className="px-5 py-2 sm:mt-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300 cursor-pointer">
         Sign In
       </button>
     </nav>

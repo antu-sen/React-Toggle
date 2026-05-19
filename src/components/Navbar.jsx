9915481cf4b916b7f81794acbc8d453ca7030439
Antu Sen <antusen787@gmail.com>
2026-05-19 12:41:18 +0600

    Added NavBar Desktop View

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import Link from "./NavBar/Link";
+
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/home",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/Blog",
+  },
+
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex justify-center items-center mt-2">
+        {navigationData.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

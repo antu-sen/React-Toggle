eb3f1e59b0634ada985ebe2acf31938502f42951
Antu Sen <antusen787@gmail.com>
2026-05-19 12:58:48 +0600

    add button or style navbar

--- src/components/NavBar/Link.jsx
+++ src/components/NavBar/Link.jsx
@@ -2,7 +2,7 @@
 const Link = ({route}) => {
     return (
         <li className="mr-12">
-            <a href={route.path}>{route.name}</a>
+            <a className="text-gray-700 font-medium hover:text-blue-600 transition duration-300" href={route.path}>{route.name}</a>
         </li>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,15 +31,18 @@ const navigationData = [
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex justify-between items-center py-2 px-20">
       <h3 className="text-3xl font-extrabold tracking-wide text-blue-600 cursor-pointer">
         My<span className="text-black">NavBar</span>
       </h3>
-      <ul className="flex justify-center items-center mt-2">
+      <ul className="flex justify-center items-center">
         {navigationData.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
       </ul>
+      <button className="px-5 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300 cursor-pointer">
+        Sign In
+      </button>
     </nav>
   );
 };

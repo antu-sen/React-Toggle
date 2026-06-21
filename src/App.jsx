baac0c5345cfb7ce75bcf19c194cd3e34527bd53
Antu Sen <antusen787@gmail.com>
2026-06-21 16:06:01 +0600

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { Suspense } from "react";
-import Navbar from "./components/Navbar";
-import PricingOption from "./components/PricingOption/PricingOption";
-import ResultChart from "./components/ResultChart";
-
-const pricingPromise = fetch("PricingData.json").then((response) =>
-  response.json()
-);
 function App() {
   return (
-    <>
-      <header>
-        <Navbar />
-        
-      </header>
-      <main>
-        <Suspense
-          fallback={
-          <span className="loading loading-spinner text-error"></span>
-          }>
-          <PricingOption pricingPromise={pricingPromise}></PricingOption>
-        </Suspense>
-
-        <ResultChart></ResultChart>
-      </main>
-    </>
-  );
+    <div>
+     <p className="text-2xl font-extrabold text-center text-amber-500">Hello world!</p>
+    </div>
+    
+  )
 }
 
 export default App;
